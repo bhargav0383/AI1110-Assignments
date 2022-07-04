1251bc5f144b5c189cbd6c17ce8f914e29b93986
bhargav0383 <cs21btech11052@iith.ac.in>
2022-07-04 09:34:43 +0530

    completed upto 3
    upto 3

--- RandomNumbers/codes/2.2CDF.py
+++ RandomNumbers/codes/2.2CDF.py
@@ -10,8 +10,7 @@ maxlim=6.0
 x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-pdf = [] #declaring pdf list
-h = 2*maxlim/(maxrange-1);
+
 
 randvar = np.loadtxt('gau.dat',dtype='double')
 
@@ -19,14 +18,17 @@ for i in range(0,maxrange):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
-	
-for i in range(0,maxrange-1):
-	test = (err[i+1]-err[i])/(x[i+1]-x[i])
-	pdf.append(test) #storing the pdf values in a list
 
-rv = norm(0, 1)
+def Q(x):
+    return (1-mp.erf(x/mp.sqrt(2)))/2
+    
+def f(x):
+    return 1-Q(x)	
+
+cdf = np.vectorize(f)
+
 plt.plot(x,err,'bo',color="blue")
-plt.plot(x, rv.cdf(x),color='orange')
+plt.plot(x,cdf(x),color='orange')
 plt.grid() #creating the grid
 plt.xlabel('$x_i$')
 plt.ylabel('$F_X(x_i)$')
