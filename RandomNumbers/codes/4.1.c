d4e2af1bb9f318f24545199d519b3eea0d77c4d5
bhargav0383 <cs21btech11052@iith.ac.in>
2022-07-07 23:13:05 +0530

    update4
    update4

---
+++ RandomNumbers/codes/4.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "functions.h"
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+sumfunc("sum.dat",1000000);
+return 0;
+}
+
