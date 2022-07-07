ef30ae8c331117ac5a5ebead1d38f2345ab27ef7
bhargav0383 <cs21btech11052@iith.ac.in>
2022-07-07 23:18:41 +0530

    update4i
    update4i

--- RandomNumbers/codes/1.2CDF.py
+++ RandomNumbers/codes/1.2CDF.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib
 matplotlib.rcParams['backend'] = 'GTK3Agg'
 import matplotlib.pyplot as plt
+import scipy
 
 x = np.linspace(-4,4,80)#points on the x axis
 #Theory graph
 #Obtained Theory graph points using function
-
 def uniCdf(x):
     if (x < 0): return 0
     elif (0 <= x < 1): 
         return x*1.0
     else: return 1
 
-  func = scipy.vectorize(uni_cdf, otypes=['double'])
+func = scipy.vectorize(uniCdf, otypes=['double'])
 
 #Analytical Graph
 simlen = int(1e6) #number of samples
--- RandomNumbers/codes/functions.h
+++ RandomNumbers/codes/functions.h
@@ -3,6 +3,7 @@ void uniform(char *str,int len);
 double mean(char *str);
 double meansqr(char *str);
 void gaussian(char *str, int len);
+void sumfunc(char *tri,int len);
 //end declaration
 
 
@@ -89,4 +90,30 @@ fprintf(fp,"%lf\n",temp);
 }
 fclose(fp);
 
+}
+
+//Function to generate the sum of two uniform distributions
+void sumfunc(char *sum,int len)
+{
+int i;
+double x,y;
+FILE *fp,*fp2,*destinyfile;
+//get two uniform distributions 
+uniform("uni1.dat",len);
+uniform("uni2.dat",len);
+//open the two files
+fp = fopen("uni1.dat","r");
+fp2=fopen("uni2.dat","r");
+destinyfile=fopen(sum,"w");
+//Generate numbers
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+fscanf(fp2,"%lf",&y);
+fprintf(destinyfile,"%lf\n",(double)(x+y));
+}
+fclose(fp);
+fclose(fp2);
+fclose(destinyfile);
+remove("uni1.dat");
+remove("uni2.dat");
 }
