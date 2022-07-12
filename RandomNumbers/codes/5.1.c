4687a1ea656d9c09159a2224a16cf2dfd81aa546
bhargav0383 <cs21btech11052@iith.ac.in>
2022-07-12 09:44:55 +0530

    5
    5

---
+++ RandomNumbers/codes/5.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "functions.h"
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+equiprobable("equi.dat",1000000);
+return 0;
+}
+
--- RandomNumbers/codes/functions.h
+++ RandomNumbers/codes/functions.h
@@ -4,6 +4,10 @@ double mean(char *str);
 double meansqr(char *str);
 void gaussian(char *str, int len);
 void sumfunc(char *tri,int len);
+void equiprobable(char *str,int len);
+int signum(double a);
+void combo(char *str,char *str2,char *str3,int len);
+double estimator(char *str1,char *str2,int len,int req);
 //end declaration
 
 
@@ -116,4 +120,92 @@ fclose(fp2);
 fclose(destinyfile);
 remove("uni1.dat");
 remove("uni2.dat");
+}
+
+int signum(double a)
+{
+    if(a>=0)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+//Function to generate equiprobable distribution belongs to {-1,1}
+void equiprobable(char *str,int len)
+{
+    int i;
+    FILE *fp;
+    fp=fopen(str,"w");
+    for (i = 0; i < len; i++)
+    {
+    fprintf(fp,"%d\n",signum(2*(double)rand()/(double)RAND_MAX - 1));
+    }
+    fclose(fp);
+}
+
+void combo(char *str,char *str2,char *str3,int len)
+{
+  //Y=AX+N
+int i,j,equi;//equi refers to numbers in BErnoulli
+double temp;//temp referes to numbers in gaussian
+FILE *fp,*gaufile,*equifile;
+fp = fopen(str,"w");//destiny file
+gaussian(str2,len);//genererate N
+equiprobable(str3,len);//generate X
+gaufile=fopen(str2,"r");
+equifile=fopen(str3,"r");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fscanf(equifile,"%d",&equi);
+fscanf(gaufile,"%lf",&temp);
+fprintf(fp,"%lf\n",0.5*equi+temp);
+}
+fclose(fp);
+}
+
+double  estimator(char *str1,char *str2,int len,int req)
+{
+// str1 represents AX+N //combo
+// str2 reprsenst X brnoulii equiprob
+int i;
+long double temp1,temp2,counter1=0,counter2=0,counter=0;
+//counter represents X=1
+combo(str1,"gau.dat","equi.dat",len);
+equiprobable(str2,len);
+FILE *fp1,*fp2;
+fp1=fopen(str1,"r");// AX+N
+fp2=fopen(str2,"r");//Bernoullii
+//calculation of P(e|0)
+for(i=0;i<len;i++)
+{
+  fscanf(fp1,"%Lf",&temp1);
+  fscanf(fp2,"%Lf",&temp2);
+  if(temp2==1)
+  {
+    counter++;
+  }
+  if(temp1<0&&temp2==1)
+  {
+    counter1++;
+  }
+  if(temp1>0&&temp2==-1)
+  {
+    counter2++;
+  }
+}
+fclose(fp1);
+fclose(fp2);
+if(req==1)
+{
+  return (double)counter1/counter;
+}
+else
+{
+  return (double)counter2/(len-counter);
+}
 }
